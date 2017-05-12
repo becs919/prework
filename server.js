591b39c0bf8c37c794c81b8c32f50bb0efee9754
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-05-11 21:46:23 -0600

    add cookies

--- server.js
+++ server.js
@@ -1,146 +1,55 @@
-// // // // // var express = require('express');
-// // // // // var app = express();
-// // // // //
-// // // // // app.get('/', function (req, res) {
-// // // // //    res.send('Hello World');
-// // // // // })
-// // // // //
-// // // // // var server = app.listen(8081, function () {
-// // // // //    var host = server.address().address
-// // // // //    var port = server.address().port
-// // // // //
-// // // // //    console.log("Example app listening at http://%s:%s", host, port)
-// // // // // })
-// // // //
-// // // //
-// // // // var express = require('express');
-// // // // var app = express();
-// // // //
-// // // // // This responds with "Hello World" on the homepage
-// // // // app.get('/', function (req, res) {
-// // // //    console.log("Got a GET request for the homepage");
-// // // //    res.send('Hello GET');
-// // // // })
-// // // //
-// // // // // This responds a POST request for the homepage
-// // // // app.post('/', function (req, res) {
-// // // //    console.log("Got a POST request for the homepage");
-// // // //    res.send('Hello POST');
-// // // // })
-// // // //
-// // // // // This responds a DELETE request for the /del_user page.
-// // // // app.delete('/del_user', function (req, res) {
-// // // //    console.log("Got a DELETE request for /del_user");
-// // // //    res.send('Hello DELETE');
-// // // // })
-// // // //
-// // // // // This responds a GET request for the /list_user page.
-// // // // app.get('/list_user', function (req, res) {
-// // // //    console.log("Got a GET request for /list_user");
-// // // //    res.send('Page Listing');
-// // // // })
-// // // //
-// // // // // This responds a GET request for abcd, abxcd, ab123cd, and so on
-// // // // app.get('/ab*cd', function(req, res) {
-// // // //    console.log("Got a GET request for /ab*cd");
-// // // //    res.send('Page Pattern Match');
-// // // // })
-// // // //
-// // // // var server = app.listen(8081, function () {
-// // // //
-// // // //    var host = server.address().address
-// // // //    var port = server.address().port
-// // // //
-// // // //    console.log("Example app listening at http://%s:%s", host, port)
-// // // // })
-// // //
-// // //
-// // // var express = require('express');
-// // // var app = express();
-// // //
-// // // app.use(express.static('public'));
-// // //
-// // // app.get('/', function (req, res) {
-// // //    res.send('Hello World');
-// // // })
-// // //
-// // // var server = app.listen(8081, function () {
-// // //    var host = server.address().address
-// // //    var port = server.address().port
-// // //
-// // //    console.log("Example app listening at http://%s:%s", host, port)
-// // //
-// // // })
-// //
-// // var express = require('express');
-// // var app = express();
-// //
-// // app.use(express.static('public'));
-// // app.get('/index.htm', function (req, res) {
-// //    res.sendFile( __dirname + "/" + "index.htm" );
-// // })
-// //
-// // app.get('/process_get', function (req, res) {
-// //    // Prepare output in JSON format
-// //    response = {
-// //       first_name:req.query.first_name,
-// //       last_name:req.query.last_name
-// //    };
-// //    console.log(response);
-// //    res.end(JSON.stringify(response));
-// // })
-// //
-// // var server = app.listen(8081, function () {
-// //    var host = server.address().address
-// //    var port = server.address().port
-// //    console.log("Example app listening at http://%s:%s", host, port)
-// //
-// // })
-//
-// var express = require('express');
-// var app = express();
-// var bodyParser = require('body-parser');
-//
-// // Create application/x-www-form-urlencoded parser
-// var urlencodedParser = bodyParser.urlencoded({ extended: false })
-//
-// app.use(express.static('public'));
-// app.get('/index.htm', function (req, res) {
-//    res.sendFile( __dirname + "/" + "index.htm" );
-// })
-//
-// app.post('/process_post', urlencodedParser, function (req, res) {
-//    // Prepare output in JSON format
-//    response = {
-//       first_name:req.body.first_name,
-//       last_name:req.body.last_name
-//    };
-//    console.log(response);
-//    res.end(JSON.stringify(response));
-// })
-//
-// var server = app.listen(8081, function () {
-//    var host = server.address().address
-//    var port = server.address().port
-//
-//    console.log("Example app listening at http://%s:%s", host, port)
-//
-// })
-
-
 var express = require('express');
 var app = express();
 var fs = require("fs");
+// var cookieParser = require('cookie-parser')
 
 var bodyParser = require('body-parser');
 var multer  = require('multer');
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.use(express.static('public'));
+// app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ dest: '/tmp/'}));
 
 app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
+  res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+// app.get('/', function (req, res) {
+//    console.log("Cookies", req.cookies);
+// })
+
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+app.get('/ab*cd', function(req, res) {
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
 })
 
 app.post('/file_upload', function (req, res) {
@@ -163,6 +72,21 @@ app.post('/file_upload', function (req, res) {
          res.end( JSON.stringify( response ) );
       });
    });
+});
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
 })
 
 var server = app.listen(8081, function () {
